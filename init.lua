e818217daa27ef3c45c842eb742dff85339fdbcc
Facundo Figueroa <facundof13@gmail.com>
2025-11-06 12:25:25 -0500

    main

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,40 +21,44 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.g.camelcasemotion_key = "<leader>"
+vim.keymap.set("n", "<leader>/", ":noh<CR>")
+
 -- Search settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
--- PLUGINS GO HERE
-  spec = {
-	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({ })
-		end
+	-- PLUGINS GO HERE
+	spec = {
+		{
+			"kylechui/nvim-surround",
+			version = "^3.0.0",
+			event = "VeryLazy",
+			config = function()
+				require("nvim-surround").setup({})
+			end,
+		},
+		{ "https://github.com/justinmk/vim-sneak" },
+		{ "https://github.com/bkad/CamelCaseMotion" },
 	},
-	{ "https://github.com/justinmk/vim-sneak" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = {--[[  colorscheme = { "habamax" }  ]]
+	},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
-
 -- highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight_yank', {}),
-    desc = "Highlight selection on yank",
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 250}
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Highlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 250 })
+	end,
 })
 
 vim.opt.clipboard:append("unnamedplus")
